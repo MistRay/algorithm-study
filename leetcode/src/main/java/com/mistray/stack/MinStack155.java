33fcdd133db2a72583c0e4318bbb9e707d7a90c0
junyi <junyizhang5420@aliyun.com>
2021-03-03 13:39:54 +0800

    移动155

--- leetcode/src/main/java/com/mistray/stack/MinStack155.java
+++ leetcode/src/main/java/com/mistray/stack/MinStack155.java
@@ -1,13 +1,77 @@
 package com.mistray.stack;
 
+import java.util.Stack;
+
 /**
  * @author ZJY(MistRay)
  * @Project algorithm-study
  * @Package com.mistray.stack
  * @create 2020年05月21日 14:33
  * @Desc
+ * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+ *
+ * push(x) —— 将元素 x 推入栈中。
+ * pop() —— 删除栈顶的元素。
+ * top() —— 获取栈顶元素。
+ * getMin() —— 检索栈中的最小元素。
+ *  
+ *
+ * 示例:
+ *
+ * 输入：
+ * ["MinStack","push","push","push","getMin","pop","top","getMin"]
+ * [[],[-2],[0],[-3],[],[],[],[]]
+ *
+ * 输出：
+ * [null,null,null,null,-3,null,0,-2]
+ *
+ * 解释：
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> 返回 -3.
+ * minStack.pop();
+ * minStack.top();      --> 返回 0.
+ * minStack.getMin();   --> 返回 -2.
+ *  
+ *
+ * 提示：
+ *
+ * pop、top 和 getMin 操作总是在 非空栈 上调用。
+ *
  */
 public class MinStack155 {
 
+    private Stack<Integer> dataStack;
+    private Stack<Integer> minStack;
 
+    // 思路:
+    // 使用辅助栈的方式,让取最小值的时间复杂度保持在O(1)
+    /** initialize your data structure here. */
+    public MinStack155() {
+        dataStack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        dataStack.push(x);
+        if (minStack.isEmpty() || minStack.peek() >= x){
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        if (dataStack.pop().equals(minStack.peek()) && minStack.size() > 1){
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return dataStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
 }
